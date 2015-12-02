cf9364549d9c20c41e4f0b93784f7237c0afc7ef
Zac Connelly <zac@counsyl.com>
2015-12-01 19:15:33 -0800

    inital 3 problems cleanup

--- project_euler_1.py
+++ project_euler_1.py
@@ -1,7 +1,13 @@
 """
-some nifty looking list comprehension
-EXPR for VAR in SET if CONDITION
+Problem 1
+=========
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
 """
+
 multiples_of_3_or_5 = [x for x in range(0, 1000) if x % 3 == 0 or x % 5 == 0]
 total = sum(multiples_of_3_or_5)
-print total
+print(total)
--- project_euler_3.py
+++ project_euler_3.py
@@ -1,18 +1,23 @@
+"""
+Problem 3
+=========
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+
 n = 600851475143
 i = 2
 prime_factors = []
-# it's not going to have a factor larger than sqrt(n)
 while i*i < n:
-    # if i is a factor of n
     while n % i == 0:
-        # a prime factor is found
         prime_factors.append(i)
-        # divide by that prime factor
         n = n / i
     i = i+1
-# the "n" leftover may also be a prime factor if not 1
 if n > 1:
     prime_factors.append(n)
 
 largest = max(prime_factors)
-print largest
+print(largest)
