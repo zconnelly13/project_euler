c43572d4be2672b217ac4d46dfd5c1bdb3e02786
Zac Connelly <zac@counsyl.com>
2015-12-01 20:17:59 -0800

    add hidden files to gitignore and problem 7

--- project_euler_7.py
+++ project_euler_7.py
@@ -7,3 +7,19 @@ the 6th prime is 13.
 
 What is the 10,001st prime number?
 """
+
+
+def primes_sieve(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for i, isprime in enumerate(a):
+        if isprime:
+            yield i
+            for n in xrange(i*i, limit, i):
+                a[n] = False
+
+for i, j in enumerate(primes_sieve(1000000)):
+    if i == 10000:
+        print(j)
+        break
