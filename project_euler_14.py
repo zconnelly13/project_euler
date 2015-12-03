f4b7591e134f922aaab28e10b85146f0a78a092a
Zac Connelly <zac@counsyl.com>
2015-12-02 19:21:52 -0800

    problem 14

--- project_euler_14.py
+++ project_euler_14.py
@@ -4,12 +4,12 @@ Problem 14
 
 The following iterative sequence is defined for the set of positive integers:
 
-    n → n/2 (n is even)
-    n → 3n + 1 (n is odd)
+    n -> n/2 (n is even)
+    n -> 3n + 1 (n is odd)
 
 Using the rule above and starting with 13, we generate the following sequence:
 
-    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
 It can be seen that this sequence (starting at 13 and finishing at 1) contains
 10 terms. Although it has not been proved yet (Collatz Problem), it is thought
 that all starting numbers finish at 1.
@@ -26,7 +26,12 @@ def collatz(n):
             return n/2
         else:
             return (3*n) + 1
-    seq = []
-    seq.append(n)
-    while next > 1:
+    seq = [n]
+    while n > 1:
         n = next(n)
+        seq.append(n)
+    return seq
+
+chains = [len(collatz(n)) for n in range(0, 1000000)]
+maximum = chains.index(max(chains))
+print(maximum)
