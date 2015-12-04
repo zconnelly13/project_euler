cb7b460a0ef3c64f5f92e4da8f2ed56b53bd124d
Zac Connelly <zconnelly13@gmail.com>
2015-12-04 09:35:55 -0800

    problem 25 clean up a bit

--- project_euler_25.py
+++ project_euler_25.py
@@ -12,18 +12,17 @@ What is the index of the first term in the Fibonacci sequence
 to contain 1000 digits?
 """
 
-def fib_generator(limit):
-    current = 1
-    prev = 1
-    yield current
-    yield prev
-    for i in xrange(0, limit):
-        next = current + prev
-        prev = current
-        current = next
-        yield current
+upper_limit = 5000  # expertly chosen after glancing at my hand
 
-for i, n in enumerate(fib_generator(5000)):
-    if len(str(n)) == 1000:
-        print i+1
-        break
+def fib_generator(limit):
+    current, prev = 1, 0
+    for i in xrange(0, limit):
+        yield current
+        current, prev = current + prev, current
+
+def fib_digit_length_generator(limit):
+    for fib in fib_generator(limit):
+        yield len(str(fib))
+
+index = list(fib_digit_length_generator(upper_limit)).index(1000) + 1
+print index
