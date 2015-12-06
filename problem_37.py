e165e1d2eb57290f8a5502bb8bacd112a69bd7f6
Zac Connelly <zconnelly13@gmail.com>
2015-12-06 15:40:00 -0800

    problem 37

--- problem_37.py
+++ problem_37.py
@@ -12,3 +12,31 @@ from left to right and right to left.
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
+
+
+from tools import primes_sieve
+
+upper_limit = 1000000
+primes = {p: True for p in primes_sieve(upper_limit)}
+
+
+def get_truncated(n):
+    """
+    Returns a unique list of n and all of its truncated versions
+    both from the left and the right
+    """
+
+    truncated_versions = [n]
+    for i in range(1, len(str(n))):
+        truncated_versions.append(int(str(n)[i:]))
+        truncated_versions.append(int(str(n)[:(-1)*i]))
+    truncated_versions = list(set(truncated_versions))
+    return truncated_versions
+
+truncatable_primes = []
+for n in range(10, upper_limit):
+    truncated = get_truncated(n)
+    if all([primes.get(p) for p in truncated]):
+        truncatable_primes.append(n)
+
+print(sum(truncatable_primes))
